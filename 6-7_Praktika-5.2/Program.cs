0f3ac5ff23a595f0681ff712a99542af374b65cf
Malik8008 <tu60prbs1@code.edu.az>
2022-04-24 02:53:51 +0400

    Capitalize procces completed

---
+++ 6-7_Praktika-5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _6_7_Praktika_5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu1=new Student("Seferov Malik", "Programmer");
+            stu1.ShowInfo();
+            Student student = new Student("Qeyyam Yaqubov","Web");     
+            student.ShowInfo();
+        }
+    }
+}
---
+++ 6-7_Praktika-5.2/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6_7_Praktika_5._2
+{
+    public class Student
+    {
+        private  int _id;
+        public static int IdCount = 0;
+        public string FullName;
+        public string Subject;
+        private int _groupid;
+        public static int GroupCount = 1000;
+
+        public Student(string fullname,string subject)
+        {
+            IdCount++;
+            _id = IdCount;
+            GroupCount++;
+            _groupid = GroupCount;
+            FullName = fullname;
+            Subject = subject;
+            
+        }
+
+        public void GroupName(string subject)
+        {           
+            Console.WriteLine(char.ToUpper(subject[0]).ToString() + char.ToUpper(subject[1]).ToString() + GroupCount);
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {IdCount}  |  FullName:{FullName}  |  Subject:{Subject}  | GroupCount:{GroupCount}");
+        }
+    }
+}
